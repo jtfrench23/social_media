ac15e939786b0bfc5c81e12b8c5d7b3a3a45c816
Jordan French <jtf981@culver.edu>
2023-01-30 12:48:01 -0600

    added user routes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import {register} from './controllers/auth.js';
-import authRoutes from "./routes/auth.js";
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import authRoutes from './routes/auth.js';
+import {createPost} from  './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
 // Configurations
 
 const __filename = fileURLToPath(import.meta.url);
@@ -40,10 +44,13 @@ const upload=multer({storage});
 // Routes with Files
 
 app.post('/auth/register', upload.single('picture'), register);
+app.post("/posts", verifyToken, upload.single('picture'), createPost);
 
 // Routes
 
 app.use("/auth", authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 
 // Mongoose Setup
