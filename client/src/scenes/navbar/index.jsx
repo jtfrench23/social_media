d1a80be8abf53b1510c5449f49ea3f7f09ce15e4
Jordan French <jtf981@culver.edu>
2023-02-01 10:09:04 -0600

    Update index.jsx

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
             }}
         >
-            Sociopedia
+            HeadPamphlet
         </Typography>
         {isNonMobileScreens && (
             <FlexBetween
