1ac31d5fb33ee60e3319fa70838edb991cafb6bd
Jordan French <jtf981@culver.edu>
2023-02-01 10:16:26 -0600

    Update index.jsx

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
         <Typography
             fontWeight="bold"
             fontSize="clamp(1rem, 2rem, 2.25rem)"
-            color="primary"
+            color="danger"
             onClick={() => navigate("/home")}
             sx={{
             "&:hover": {
