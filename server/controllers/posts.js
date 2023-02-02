f0dce4e1ab4fa8ca7978456caa0c9ca68f21c41c
Jordan French <jtf981@culver.edu>
2023-02-02 13:52:49 -0600

    added backend add comment function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -71,3 +71,20 @@ export const likePost = async (req, res) => {
         res.status(404).json({message: err.message});
     }
 }
+
+export const commentOnPost = async (req, res) => {
+    try{
+        const{id}=req.params;
+        const {comment}=req.params;
+        const post = await Post.findById(id);
+        post.comments.append(comment);
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            {comments: post.comments},
+            {new:true}
+        );
+        res.status(200).json(updatedPost);
+    } catch(err){
+        res.status(400).json({message:err.message});
+    }
+}
